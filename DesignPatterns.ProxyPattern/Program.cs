fe19aa5d5c9eccfa63171e9a54511188db44457e
Muhammad Abdelrazik <Muha@codify365.com>
2016-08-30 01:14:25 +0200

    Adding enclosing class that contains the private subject class & the public interface that every proxy will have to implement

---
+++ DesignPatterns.ProxyPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.ProxyPattern
+{
+    public class SubjectAccessor
+    {
+        public interface ISubject
+        {
+            string Request();   
+        }
+
+        private class Subject
+        {
+            public string Request()
+            {
+                return "Subject Request : Choose Left Door";
+            }
+        }
+
+        
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
